475fae23ebd17971c5710183fae99a00137ef5a9
Auriga <katkam.siddeshwar@aurigait.com>
2022-09-01 11:40:49 +0530

    Alomfire

--- Combined Framework/ApiCaller.swift
+++ Combined Framework/ApiCaller.swift
@@ -7,17 +7,39 @@
 
 import Foundation
 import Combine
+import Alamofire
+import SwiftyJSON
 
 class ApiCaller {
     
     static let shared = ApiCaller()
+    let Api = "https://reqres.in/api/products/3"
     
     private init() { }
     
     func getData() -> Future<[String], Error> {
         return Future<[String], Error> { promixe in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                promixe(.success(["AurigaIT", "Apple", "Microsoft"]))
+            AF.request(self.Api, method: .get, parameters: [:]).responseData {
+                response in
+                switch response.result {
+                case .success:
+                    var model = [String]()
+                    let UserJSON : JSON = JSON(response.value!)
+                    let color = UserJSON["data"]["color"].stringValue
+                    let id = UserJSON["data"]["id"].stringValue
+                    let pantone_value = UserJSON["data"]["pantone_value"].stringValue
+                    let year = UserJSON["data"]["year"].stringValue
+                    let name = UserJSON["data"]["name"].stringValue
+                    model.append("Name : " + name)
+                    model.append("Id : " + id)
+                    model.append("Color : " + color)
+                    model.append("Year : " + year)
+                    model.append("Pantone Value : " + pantone_value)
+                    promixe(.success(model))
+                    break
+                case .failure(_):
+                    return
+                }
             }
         }
     }
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Combined Framework' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Combined Framework
+    pod 'Alamofire'
+    pod 'SwiftyJSON'
+
+
+  target 'Combined FrameworkTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Combined FrameworkUITests' do
+    # Pods for testing
+  end
+
+end
